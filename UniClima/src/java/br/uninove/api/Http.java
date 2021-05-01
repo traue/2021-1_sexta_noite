3ea7c902aa9e6d79c3b8a65fb0c3ba3b5e5b6686
Thiago G. Traue <t.traue@gmail.com>
2021-04-30 21:59:48 -0300

    Update no projeto

--- UniClima/src/java/br/uninove/api/Http.java
+++ UniClima/src/java/br/uninove/api/Http.java
@@ -1,7 +1,13 @@
 package br.uninove.api;
 
 import br.uninove.uniclima.Clima;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -9,6 +15,16 @@ import java.nio.charset.StandardCharsets;
 
 
 public class Http {
+    
+    private static String readAll(Reader reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while((cp = reader.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+            
     
     public static Clima getClima(String cidade) {
         try {
@@ -25,13 +41,25 @@ public class Http {
                         URLEncoder.encode(lang, charset));
         
             URLConnection conn = new URL(url + query).openConnection();
-            //PARAMOS AQUI
             
-        } catch(IOException e) {
+            conn.setRequestProperty("Accept-Charset", charset);
+            conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+            conn.setDoOutput(true);
+            
+            Clima clima;
+            
+            try(InputStream response = conn.getInputStream()) {
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(response, charset));
+                String json = readAll(buffer);
+                clima = new Gson().fromJson(json, Clima.class);
+            }
+            
+            return clima;
+            
+        } catch(JsonSyntaxException | IOException e) {
             
         }
         
         return null;
     }
-    
 }
---
+++ UniClima/web/index.jsp
@@ -0,0 +1,27 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Uni Clima</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        <style>
+            .tempo-card {
+                padding-top: 100px;
+                width: 400px;
+                margin: 0 auto !important;
+            }
+        </style>
+    </head>
+    <body>
+        
+    <!-- barra de nav-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+    <!--  paramos aqui-->
+        </div>
+    </nav>
+    
+    </body>
+</html>
